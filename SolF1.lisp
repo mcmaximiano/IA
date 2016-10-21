8fe946bf4b18cb72f3a41654d3abc199c966b8bd
NunoRibeiro185 <nunomonizribeiro@gmail.com>
2016-10-21 16:00:00 +0100

    SolF1 21/10/16 16h

--- SolF1.lisp
+++ SolF1.lisp
@@ -12,28 +12,37 @@
 ;(load "auxfuncs.fas")
 
 (defun isObstaclep (pos track) 
-  "check if there is an obstacle at position pos of the track"
-  (setf a (car pos))
-  (setf b (cadr pos))
-  (cond
-   ((eq nil (nth a (nth b track))) t)
-   (t nil))
-  t)
+	"check if there is an obstacle at position pos of the track"
+	(let ((a (car pos))(b (cadr pos)))
+		(cond
+			((equal nil (nth a (nth b track))) t)
+			(t nil))
+	t))
 
 (defun isGoalp (st) 
   "check if st is a goal state"
-  (setf a (car pos))
-  (setf b (cadr pos))
-  (cond
-   ((eq E (nth a (nth b track))) t)
-   (t nil))
-  t)
+	(let ((lst 'st-track-endposition))
+		(dolist (n lst)
+			(cond ((equal n 'st-pos) t)
+				(t nil)))
+	t))
 
 (defun nextState (st act)
   "generate the nextState after state st and action act"
-  (make-STATE :POS '(3 16)
-	      :VEL '(1 3)
-	      :ACTION act
-	      :COST -100))
-
+; (make-STATE :POS '(3 16)
+;	      :VEL '(1 3)
+;	      :ACTION act
+;	      :COST -100))
+	;let((pos 'st-pos) (vel 'st-vel)
+	"pos l = pos l + vel l + act l"
+	"pos c = pos c + vel c + act c"
+	"v l = v l + act l"
+	"v c = v c + act c"
+	"nova pos is obstacle nova pos = pos; v = 0; cost += 20"
+	"nova pos is goal cost -= 99"
+	
+	
+	
+	
+	
 
